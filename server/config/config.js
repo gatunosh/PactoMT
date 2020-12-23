96bbe674e41102c6ca433d8ca6d316ad3ef6b7f1
Adrian Lima <adrigato3@gmail.com>
2020-12-23 12:12:02 -0500

    Variable de entorno MONGO guardada en el servidor, por seguridad del repositorio

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/pacto';
 } else {
-    urlDB = 'mongodb+srv://gatunosh:SuALUSrUhLHuPMGi@pactoserver.mqhkk.mongodb.net/Pacto';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
