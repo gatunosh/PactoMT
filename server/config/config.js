9bc4d138ea67f29c53be8794fb9d5e44f331c4aa
Adrian Lima <adrigato3@gmail.com>
2020-12-25 13:16:34 -0500

    Login de usuario, verificación de token y verificación de roles

--- server/config/config.js
+++ server/config/config.js
@@ -11,6 +11,18 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // ======================
+// Vencimiento del token
+// ======================
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// ======================
+// SEED de autenticación
+// ======================
+
+process.env.SEED = process.env.SEED || 'seed-desarrollo';
+
+// ======================
 // Base de datos
 // ======================
 
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -4,11 +4,11 @@ const bcrypt = require('bcrypt');
 const _ = require('underscore');
 
 const Usuario = require('../models/usuario');
-const usuario = require('../models/usuario');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
 
 const app = express();
 
-app.get('/usuario', (req, res) => {
+app.get('/usuario', [verificaToken, verificaAdmin_Role], (req, res) => {
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -41,7 +41,7 @@ app.get('/usuario', (req, res) => {
 
 });
 
-app.post('/usuario', (req, res) => {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], (req, res) => {
 
     let body = req.body;
 
@@ -73,7 +73,7 @@ app.post('/usuario', (req, res) => {
 
 });
 
-app.put('/usuario/:id', (req, res) => {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
 
     let id = req.params.id;
 
@@ -95,7 +95,7 @@ app.put('/usuario/:id', (req, res) => {
 
 });
 
-app.delete('/usuario/:id', (req, res) => {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
 
     let id = req.params.id;
 
