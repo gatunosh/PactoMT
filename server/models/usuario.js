dcb81d5e7e862f3f9938ff9cdd69d05a0759a9f7
Adrian Lima <adrigato3@gmail.com>
2021-01-25 22:51:48 -0500

    Contraseña en el get de usuarios

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -62,12 +62,12 @@ let usuarioSchema = new Schema({
     }
 });
 
-usuarioSchema.methods.toJSON = function() {
-    let user = this;
-    let userObject = user.toObject();
-    delete userObject.password;
-    return userObject;
-}
+// usuarioSchema.methods.toJSON = function() {
+//     let user = this;
+//     let userObject = user.toObject();
+//     delete userObject.password;
+//     return userObject;
+// }
 
 usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser único' })
 
