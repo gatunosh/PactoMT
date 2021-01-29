b6c6e560d6b8d2c418aecfda4d2f943361a745fc
Ivanend13 <73257964+Ivanend13@users.noreply.github.com>
2021-01-28 21:54:14 -0500

    Create multa.js

---
+++ server/models/multa.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let multaSchema = new Schema({
+    id_soc: {
+        type: Schema.Types.ObjectId,
+        ref: "Usuario",
+        required: [true, 'El id del socio es requerido']
+    },
+    multa: {
+        type: String,
+        required: [true, 'El valor de la multa es requerido']
+    },
+    id_reu: {
+        type: Schema.Types.ObjectId,
+        ref: "Reuniones",
+        required: [true, 'El id de la reunion es requerido']
+    }
+});
+
+
+module.exports = mongoose.model('Multa', multaSchema);
