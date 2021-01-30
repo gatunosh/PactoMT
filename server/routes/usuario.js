44325c761349bf0ead0d95d87c7ddcec31adc243
Carlos Yepez <carlosy10z@hotmail.com>
2021-01-30 16:22:44 -0500

    Middleware en post de usuarios

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -54,7 +54,7 @@ app.get("/usuario/:correo", (req, res) => {
     });
 });
 
-app.post("/usuario", (req, res) => {
+app.post("/usuario", [verificaToken, verificaAdmin_Role], (req, res) => {
     let body = req.body;
 
     let usuario = new Usuario({
