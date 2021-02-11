6ed78e6b88246fbe80e7e5508e42844503cb1d04
andm13 <andrescev13@gmail.com>
2021-02-10 22:22:17 -0500

    Arreglo put usuarios

--- server/models/reuniones.js
+++ server/models/reuniones.js
@@ -2,57 +2,57 @@ const mongoose = require("mongoose");
 
 let Schema = mongoose.Schema;
 
-
 let tipo_reu_enum = {
-    values: ['Ordinaria', 'Extraordinaria'],
-    message: '{VALUE} no es un tipo de reunión válida'
+  values: ["Ordinaria", "Extraordinaria", "Otros"],
+  message: "{VALUE} no es un tipo de reunión válida",
 };
 
 let reuSchema = new Schema({
-    fec_reu: {
-        type: Date,
-        required: [true, "La fecha es requerido"],
-    },
-    hor_reu: {
-        type: String,
-        required: [true, "La hora es requerido"],
-    },
-    mul_reu: {
-        type: String,
-        required: [true, "La multa es requerido"],
-    },
-    id_asoc_reu: {
-        type: Schema.Types.ObjectId,
-        ref: "Asociacion",
-        required: [true, "La id de la asociacion es requerido"],
-    },
-    asistencia: {
-        type: [{
-            id_soc: {
-                type: Schema.Types.ObjectId,
-                ref: "Usuario",
-                required: [true, "El id del socio es requerido"],
-            },
-            asist_socio: {
-                type: Boolean,
-                required: [true, "La asistencia es requerido"],
-            },
-        }, ],
-    },
-    acta: {
-        type: String,
-        /*El acta puede ser o no requerida*/
-    },
-    tema_reun: {
-        type: String,
-        required: [true, 'El tema es requerido']
-    },
-    tipo_reun: {
-        type: String,
-        default: 'Ordinaria',
-        tipo_reu_enum: tipo_reu_enum
-    }
-
+  fec_reu: {
+    type: Date,
+    required: [true, "La fecha es requerido"],
+  },
+  hor_reu: {
+    type: String,
+    required: [true, "La hora es requerido"],
+  },
+  mul_reu: {
+    type: String,
+    required: [true, "La multa es requerido"],
+  },
+  id_asoc_reu: {
+    type: Schema.Types.ObjectId,
+    ref: "Asociacion",
+    required: [true, "La id de la asociacion es requerido"],
+  },
+  asistencia: {
+    type: [
+      {
+        id_soc: {
+          type: Schema.Types.ObjectId,
+          ref: "Usuario",
+          required: [true, "El id del socio es requerido"],
+        },
+        asist_socio: {
+          type: Boolean,
+          required: [true, "La asistencia es requerido"],
+        },
+      },
+    ],
+  },
+  acta: {
+    type: String,
+    /*El acta puede ser o no requerida*/
+  },
+  tema_reun: {
+    type: String,
+    required: [true, "El tema es requerido"],
+  },
+  tipo_reun: {
+    type: String,
+    default: "Ordinaria",
+    tipo_reu_enum: tipo_reu_enum,
+  },
 });
 
 module.exports = mongoose.model("Reuniones", reuSchema);
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -5,150 +5,153 @@ const _ = require("underscore");
 
 const Usuario = require("../models/usuario");
 const {
-    verificaToken,
-    verificaAdmin_Role,
+  verificaToken,
+  verificaAdmin_Role,
 } = require("../middlewares/autenticacion");
 
 const app = express();
 
 app.get("/usuario", [verificaToken, verificaAdmin_Role], (req, res) => {
-    let desde = req.query.desde || 0;
-    desde = Number(desde);
+  let desde = req.query.desde || 0;
+  desde = Number(desde);
 
-
-    Usuario.find({ estado: true }, "nombre apellido tlfc tlfm email role estado")
-        .sort('email')
-        .populate('id_asociacion.id_asociacion')
-        .skip(desde)
-        .exec((err, usuarios) => {
-            if (err) {
-                return res.status(400).json({
-                    ok: false,
-                    err,
-                });
-            }
-
-            Usuario.count({ estado: true }, (err, conteo) => {
-                res.json({
-                    ok: true,
-                    usuarios,
-                    total: conteo,
-                });
-            });
+  Usuario.find({ estado: true }, "nombre apellido tlfc tlfm email role estado")
+    .sort("email")
+    .populate("id_asociacion.id_asociacion")
+    .skip(desde)
+    .exec((err, usuarios) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          err,
         });
+      }
+
+      Usuario.count({ estado: true }, (err, conteo) => {
+        res.json({
+          ok: true,
+          usuarios,
+          total: conteo,
+        });
+      });
+    });
 });
 
 app.get("/usuario/:correo", (req, res) => {
-    let correo = req.params.correo;
+  let correo = req.params.correo;
 
-    Usuario.findOne({ email: correo }, (err, usuarioDB) => {
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                err,
-            });
-        }
+  Usuario.findOne({ email: correo }, (err, usuarioDB) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err,
+      });
+    }
 
-        res.json({
-            ok: true,
-            usuario: usuarioDB,
-        });
+    res.json({
+      ok: true,
+      usuario: usuarioDB,
     });
+  });
 });
 
 app.post("/usuario", [verificaToken, verificaAdmin_Role], (req, res) => {
-    let body = req.body;
+  let body = req.body;
 
-    let usuario = new Usuario({
-        nombre: body.nombre,
-        apellido: body.apellido,
-        tlfc: body.tlfc,
-        tlfm: body.tlfm,
-        email: body.email,
-        password: bcrypt.hashSync(body.password, 10),
-        role: body.role,
-        id_asociacion: body.id_asociacion
+  let usuario = new Usuario({
+    nombre: body.nombre,
+    apellido: body.apellido,
+    tlfc: body.tlfc,
+    tlfm: body.tlfm,
+    email: body.email,
+    password: bcrypt.hashSync(body.password, 10),
+    role: body.role,
+    id_asociacion: body.id_asociacion,
+  });
+
+  usuario.save((err, usuarioDB) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err,
+      });
+    }
+
+    res.json({
+      ok: true,
+      usuario: usuarioDB,
     });
-
-    usuario.save((err, usuarioDB) => {
-        if (err) {
-            return res.status(400).json({
-                ok: false,
-                err,
-            });
-        }
-
-        res.json({
-            ok: true,
-            usuario: usuarioDB,
-        });
-    });
+  });
 });
 
 app.put("/usuario/:id", [verificaToken], (req, res) => {
-    let id = req.params.id;
+  let id = req.params.id;
 
-    let body = _.pick(req.body, [
-        "nombre",
-        "apellido",
-        "tlfc",
-        "email",
-        "tlfm",
-        "hectareas",
-        "sector",
-        "barrio",
-        "parroquia",
-        "estado",
-        "role",
-    ]);
+  let body = _.pick(req.body, [
+    "nombre",
+    "apellido",
+    "tlfc",
+    "email",
+    "tlfm",
+    "hectareas",
+    "sector",
+    "barrio",
+    "parroquia",
+    "estado",
+    "role",
+    "id_asociacion",
+  ]);
 
-    Usuario.findByIdAndUpdate(
-        id,
-        body, { new: true, runValidators: true },
-        (err, usuarioDB) => {
-            if (err) {
-                return res.status(400).json({
-                    ok: false,
-                    err,
-                });
-            }
+  Usuario.findByIdAndUpdate(
+    id,
+    body,
+    { new: true, runValidators: true },
+    (err, usuarioDB) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          err,
+        });
+      }
 
-            res.json({
-                ok: true,
-                usuario: usuarioDB,
-            });
-        }
-    );
+      res.json({
+        ok: true,
+        usuario: usuarioDB,
+      });
+    }
+  );
 });
 
 app.delete("/usuario/:id", [verificaToken, verificaAdmin_Role], (req, res) => {
-    let id = req.params.id;
+  let id = req.params.id;
 
-    Usuario.findByIdAndUpdate(
-        id, { estado: false }, { new: true },
-        (err, usuarioBorrado) => {
-            if (err) {
-                return res.status(400).json({
-                    ok: false,
-                    err,
-                });
-            }
+  Usuario.findByIdAndUpdate(
+    id,
+    { estado: false },
+    { new: true },
+    (err, usuarioBorrado) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          err,
+        });
+      }
 
-            if (!usuarioBorrado) {
-                return res.status(400).json({
-                    ok: false,
-                    err: {
-                        message: "El usuario no fue encontrado",
-                    },
-                });
-            }
+      if (!usuarioBorrado) {
+        return res.status(400).json({
+          ok: false,
+          err: {
+            message: "El usuario no fue encontrado",
+          },
+        });
+      }
 
-            res.json({
-                ok: true,
-                usuario: usuarioBorrado,
-            });
-        }
-    );
+      res.json({
+        ok: true,
+        usuario: usuarioBorrado,
+      });
+    }
+  );
 });
 
 module.exports = app;
